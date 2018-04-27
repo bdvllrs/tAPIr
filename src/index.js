efe7efcfe65e3aa52d700f3e896d0a6305d6220d
bdvllrs <bdvllrs@gmail.com>
2018-04-27 16:26:33 +0200

    Edit index import file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const { Controller } = require('./Controller');
 const { Request } = require('./Request');
 const { Response } = require('./Response/Response');
 const { View } = require('./Response/View');
+const {router, Router} = require('./Router');
+const {GetRoute, PostRoute, PutRoute, DeleteRoute} = require('./Router');
+
+const {walkDirectory, loadConfig} = require('./utils');
 
 exports.Controller = Controller;
 
@@ -9,3 +13,18 @@ exports.Request = Request;
 
 exports.Response = Response;
 exports.View = View;
+
+// Utils
+exports.walkDirectory = walkDirectory;
+exports.loadConfig = loadConfig;
+
+// Router
+exports.Router = Router;
+
+exports.router = router;
+
+exports.GetRoute = GetRoute;
+exports.PostRoute = PostRoute;
+exports.PutRoute = PutRoute;
+exports.DeleteRoute = DeleteRoute;
+
